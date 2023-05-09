b99e8647c8a17f3ece4b905503d0bb3b1747c533
Muhammad Hasbi <onlyhasbi@gmail.com>
2023-05-09 10:12:45 +0800

    fix references key playlist song activities

--- migrations/1683177434552_create-table-playlist-song-activities.js
+++ migrations/1683177434552_create-table-playlist-song-activities.js
@@ -7,13 +7,23 @@ exports.up = (pgm) => {
       type: 'VARCHAR(50)',
       notNull: true,
     },
-    song_id: { type: 'VARCHAR(50)' },
-    user_id: { type: 'VARCHAR(50)' },
+    song_id: { type: 'VARCHAR(50)', notNull: true },
+    user_id: { type: 'VARCHAR(50)', notNull: true },
     action: { type: 'TEXT', notNull: true },
     time: { type: 'TEXT', notNull: true },
   });
+
+  pgm.addConstraint(
+    'playlist_song_activities',
+    'fk_playlist_song_activities.playlist_id:playlists.id',
+    'FOREIGN KEY(playlist_id) REFERENCES playlists(id) ON DELETE CASCADE'
+  );
 };
 
 exports.down = (pgm) => {
+  pgm.dropConstraint(
+    'playlist_song_activities',
+    'fk_playlist_song_activities.playlist_id:playlists.id'
+  );
   pgm.dropTable('playlist_song_activities');
 };
